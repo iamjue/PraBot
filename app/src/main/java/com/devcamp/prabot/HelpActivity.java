196b310ab686a42c62eb3d6a00af69a040c0db10
etanadya <nadyanurarrosy@gmail.com>
2019-07-25 00:34:06 +0700

    update UI

--- app/src/main/java/com/devcamp/prabot/HelpActivity.java
+++ app/src/main/java/com/devcamp/prabot/HelpActivity.java
@@ -1,6 +1,28 @@
 package com.devcamp.prabot;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
 
 public class HelpActivity extends AppCompatActivity {
+    CardView cardView;
+    Button btnSelesai;
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_help);
+
+        cardView = (CardView)findViewById(R.id.cardview_help);
+
+        btnSelesai = (Button)findViewById(R.id.btn_selesai);
+        btnSelesai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveTaskToBack(true);
+            }
+        });
+    }
 }
